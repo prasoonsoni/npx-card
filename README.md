6250ca115e6b89a1a9c1d91baf6c4cd2d9a3f4e3
Prasoon Soni <prasoonsoni03@gmail.com>
2022-12-06 14:18:08 +0530

    Added Card

---
+++ README.md
@@ -0,0 +1,9 @@
+# NPX CARD
+This my NPX card unique style to connect with me directly via console or terminal
+
+👇 Just Hit 
+```bash
+npx prasoon
+```
+And get to know me in unique style.
+
---
+++ card.js
@@ -0,0 +1,121 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("E-Mail")}?`,
+                value: () => {
+                    open("mailto:contact@prasoon.codes");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+                value: () => {
+                    // cliSpinners.dots;
+                    const loader = ora({
+                        text: ' Downloading Resume',
+                        spinner: cliSpinners.material,
+                    }).start();
+                    let pipe = request('https://prasoon.codes/Resume-PrasoonSoni.pdf').pipe(fs.createWriteStream('./Resume-PrasoonSoni.pdf'));
+                    pipe.on("finish", function () {
+                        let downloadPath = path.join(process.cwd(), 'Resume-PrasoonSoni.pdf')
+                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
+                        open(downloadPath)
+                        loader.stop();
+                    });
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.rgb(59,52,134)("                     PRASOON SONI"),
+    handle: chalk.white("@prasoonsoni"),
+    work: `${chalk.white("Upcoming AEH Intern at")} ${chalk
+        .hex("#2b82b2")
+        .bold("Accenture")}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("prasoonsoni_"),
+    github: chalk.gray("https://github.com/") + chalk.green("prasoonsoni"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("prasoonsoni"),
+    web: chalk.cyan("https://prasoon.codes"),
+    npx: chalk.red("npx") + " " + chalk.white("prasoon"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("  Portfolio:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am currently looking for new opportunities,"
+        )}`,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "round",
+        borderColor: "#332FD0"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
